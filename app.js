530800bb886e256fd0b8486c9e914d93c7f325bb
This-is-Luke <lluminous.210@gmail.com>
2023-03-25 20:00:53 +0200

    added a modal button for form input and list display

--- app.js
+++ app.js
@@ -116,6 +116,30 @@ function displayMeals()
   });
   });
 }
+
+// Get the modal box, button to open it, and the close button
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("openModalBtn");
+const span = document.getElementsByClassName("close")[0];
+
+// When the button is clicked, show the modal box
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on the close button, hide the modal box
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, hide it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 window.addEventListener("load", () => {
   displayMeals();
   setTimeout(() => {
